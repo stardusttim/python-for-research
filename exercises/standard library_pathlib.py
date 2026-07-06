5d03a51dc37ef9de8fa897c1ad31dea7fa454eb4
Stardust <13539789887@qq.com>
2026-07-06 15:50:13 +0800

    class basic and pathlib application

---
+++ exercises/standard library_pathlib.py
@@ -0,0 +1,29 @@
+# pathlib
+from pathlib import Path
+
+p = Path('.')  # current directory
+
+print([x for x in p.iterdir() if x.is_dir()])
+
+print(list(p.glob('**/*.py')))
+
+p = Path('C:/Windows')  # Windows
+q = p / 'notepad.exe'
+
+print(q)
+
+print(q.resolve())
+
+print(q.exists())
+print(q.is_dir())
+
+p = Path('.') / 'exercises' / 'test.txt'
+p.write_text('This is a test.\nGood', encoding='utf-8')
+
+with p.open(encoding='utf-8') as f: 
+    print(f.readline())
+    
+with p.open(encoding='utf-8') as f: 
+    print(f.read())
+    
+print(p.read_text(encoding='utf-8'))
